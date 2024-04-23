bbbc4b06a632208eaad57b81f5d56ed86686ea3b
RaMadaSilva <raul01mateia@gmail.com>
2024-04-23 17:52:34 +0100

    implementando o repositorio e o context

---
+++ src/MayTheFouthBackend.Infra.Data/Contexts/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using MayTheFouthBackend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MayTheFouthBackend.Infra.Data.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Planet> Planets { get; set; }
+    }
+}
---
+++ src/MayTheFouthBackend.Infra.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using MayTheFouthBackend.Domain.Entities;
+using MayTheFouthBackend.Domain.Interfaces.IRepositories;
+using MayTheFouthBackend.Infra.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace MayTheFouthBackend.Infra.Data.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : Entity
+    {
+        private readonly ApplicationContext _context;
+        private readonly DbSet<T> _set;
+
+        public BaseRepository(ApplicationContext context)
+        {
+            _context = context;
+            _set = _context.Set<T>();
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+           var entity = await _set.FindAsync(id);
+            if(entity is null)
+                return false;
+
+            _set.Remove(entity); 
+            return true;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _set.AsNoTracking().ToListAsync(); 
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _set.FindAsync(id); 
+        }
+
+        public async Task<bool> UpdateAsync(T entity)
+        {
+            await Task.CompletedTask; 
+           _set.Entry(entity).State = EntityState.Modified;
+            return true;
+        }
+    }
+}
