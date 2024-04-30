f9474052d9c8a1267c704a2e0399eca6198e1577
RaMadaSilva <raul01mateia@gmail.com>
2024-04-30 14:26:38 +0100

    resolvendo um problema de paginação

--- src/MayTheFouthBackend.Application/Mappers/IEnumerableExtension.cs
+++ src/MayTheFouthBackend.Application/Mappers/IEnumerableExtension.cs
@@ -4,13 +4,8 @@ namespace MayTheFouthBackend.Application.Mappers;
 
 public static class IEnumerableExtension
 {
-    private const int defaultCurrentPage = 1;
-    private const int defaulPageSize = 10;
-
     public static PaginatedListResponse<T> ToPaginationList<T>(this IEnumerable<T> source, int currentPage, int pageSize) where T : class
     {
-        currentPage = currentPage > 0 ? currentPage : defaultCurrentPage;
-        pageSize = pageSize > 0 ? pageSize : defaulPageSize;
         var totalItems = source.Count();
         var items = source.Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
 
